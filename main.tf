6255c38f62eaf0dc0a81118b3c222dfcda8fab10
Mateus Henrique Calza <mateushenrique17@gmail.com>
2022-03-30 19:14:42 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,6 +11,8 @@ locals {
     subnet_cidr_block = local.public_enabled ? aws_subnet.public[az].cidr_block : aws_subnet.private[az].cidr_block
     route_table_id    = local.public_enabled ? aws_route_table.public[az].id : aws_route_table.private[az].id
     ngw_id            = local.public_enabled && var.nat_gateway_enabled ? aws_nat_gateway.public[az].id : null
+    eip_id            = local.public_enabled ? aws_eip.public[az].id : null
+    eip_public_ip     = local.public_enabled ? aws_eip.public[az].public_ip : null
     }
   }
 }
